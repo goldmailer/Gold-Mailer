7e04c64090d1840d8fe695d224c52202811f3f05
Task Nest <yahuzaalmajir2@gmail.com>
2026-09-26 07:19:18 +0000

    Update service worker sw.js with Monetag zone 11894953

--- artifacts/gold-mailer/public/sw.js
+++ artifacts/gold-mailer/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11866172
+    "zoneId": 11894953
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
